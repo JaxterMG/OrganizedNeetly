26caea7143f9a32dfc9b1a16f68b5c01610420fa
JaxterMG <46417741+JaxterMG@users.noreply.github.com>
2024-01-09 13:29:25 +0300

    ratio scaler for mobile or tablet

---
+++ Assets/GAssets/Scripts/Camera/RatioScaler.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class RatioScaler : MonoBehaviour
+{
+    private const float MOBILEFOV = 4.7f;
+    private const float TABLETFOV = 3.8f;
+    void OnEnable()
+    {
+        Camera camera = GetComponent<Camera>();
+        camera.orthographicSize = IsTablet()? TABLETFOV : MOBILEFOV;
+    }
+    bool IsTablet()
+    {
+        // Получаем размеры экрана
+        float screenWidth = Screen.width / Screen.dpi;
+        float screenHeight = Screen.height / Screen.dpi;
+        float size = Mathf.Sqrt(screenWidth * screenWidth + screenHeight * screenHeight);
+
+        // Предположим, что устройства с диагональю больше 6.5 дюймов - это планшеты
+        return size >= 6.5f;
+    }
+}
