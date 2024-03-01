d71922316597412650bff20492df5e2e165cad02
JaxterMG <46417741+JaxterMG@users.noreply.github.com>
2024-03-01 13:14:23 +0300

    Money object pool

--- Assets/GAssets/Scripts/Shop/MoneyManager/MoneyAttractorView.cs
+++ Assets/GAssets/Scripts/Shop/MoneyManager/MoneyAttractorView.cs
@@ -5,6 +5,7 @@ public class MoneyAttractorView : MonoBehaviour
 {
     [SerializeField] private GameObject _moneyObject;
     [SerializeField] float _transitionDuration = 0.5f;
+    [SerializeField] private ObjectPooler _objectPooler;
     public void RequestViewUpdate(Vector3 position)
     {
         Vector3 canvasPosition = Camera.main.WorldToScreenPoint(position);
@@ -13,8 +14,11 @@ public class MoneyAttractorView : MonoBehaviour
     }
     private GameObject CreateMoneyImageElement(Vector3 canvasPosition)
     {
-        GameObject moneyImage = Instantiate(_moneyObject, transform);
+        GameObject moneyImage =  _objectPooler.RequestMoneyObject();
+        moneyImage.SetActive(true);
+        moneyImage.transform.SetParent(transform);
         moneyImage.transform.position = canvasPosition;
+
         return moneyImage;
     }
     public void AttractMoneyImageElement(GameObject moneyObject)
@@ -22,7 +26,11 @@ public class MoneyAttractorView : MonoBehaviour
         moneyObject.transform.DOMove(transform.position - new Vector3(0, 2.6f, 0), _transitionDuration)
         .OnComplete(() => moneyObject.transform.DOScale(1.3f, _transitionDuration / 4).SetEase(Ease.Linear)
         .OnComplete(() => moneyObject.transform.DOScale(0f, _transitionDuration / 4).SetEase(Ease.Linear)
-        .OnComplete(() => Destroy(moneyObject))));
+        .OnComplete(() => 
+        {
+            _objectPooler.RemoveMoneyObject(moneyObject);
+            moneyObject.transform.localScale = Vector3.one;
+        })));
         moneyObject.transform.DORotate(new Vector3(0,0, 360), _transitionDuration, RotateMode.FastBeyond360);
     }
 }
